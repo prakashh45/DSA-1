4ad11f1d05c575d4ada5c2908dc8fd17dee42dba
PRAKASH PATIL <patilprakash0235@gmail.com>
2026-03-01 22:04:06 +0530

    DSA - first push

---
+++ Assi-1/p2.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i = 1 ; i <= 4 ; i++){
+			for(int j = 1 ; j <= 4 ; j++){
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//  1 1 1 1 
+// 2 2 2 2 
+// 3 3 3 3 
+// 4 4 4 4 
---
+++ Assi-3/p2.java
@@ -0,0 +1,23 @@
+class Demo{
+	public static void main(String[]args){
+
+	int n = 5;
+
+	for(int i = 1 ; i <= n ; i++){
+		for(int j = 1 ; j <= n ; j++){
+			if(i == j || (i+j) == n+1){
+				System.out.print(i+" ");
+				}else{
+				System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+}
+
+// 1    1 
+//  2  2  
+//   3   
+//  4  4  
+// 5    5
